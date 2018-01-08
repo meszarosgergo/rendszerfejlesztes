406fcdcde3b4dc6189450bcdeae5092193705f18
Mészáros Gergő <meszaros.gergo@outlook.hu>
2018-01-09 00:35:12 +0100

    Felhasználói adatok frissítése A letiltás még nem jó, jelszómódositás admin felületről még nincs kész

--- intranet/adminisztracio.php
+++ intranet/adminisztracio.php
@@ -31,14 +31,33 @@ if($_SESSION['role_id'] <= 1)
 	echo "<table width=100% border=1><tr>";
 	echo "<tr><td>Név</td><td>Felhasználónév</td><td>Email cím</td><td>Szerepkör</td><td>Tiltott</td><td></td></tr>";
 	while (list($ID, $name, $username, $email, $role_id, $disabled)=mysqli_fetch_row($result)){     
-	  echo "<tr><input type=hidden name=id value=$ID><td>$name</td><td>$username</td><td>$email</td><td>".roleCheck($role_id)."</td><td>".disabledCheck($disabled)."</td><td><input type=submit value=Módosít></td></tr>";
+		echo "
+		<tr>
+			<form action=edit_user.php method=post>
+				<input type=hidden name=ID value=$ID>
+				<td><input type=hidden name=name value=$name>$name</td>
+				<td><input type=hidden name=username value=$username>$username</td>
+				<td><input type=hidden name=email value=$email>$email</td>
+				<td><input type=hidden name=role value=$role_id>".roleCheck($role_id)."</td>
+				<td>";
+				if(!$disabled)
+				{
+					echo"<input type=checkbox name=disabled value=$disabled>";
+				}
+				else
+				{
+					echo"<input type=checkbox name=disabled value=$disabled checked>";
+				}
+				echo"</td>
+				<td><input type=submit value=Szerkesztés></td>
+			</form>
+		</tr>";
 	}
 	
 	echo "</form></table>";
 	echo"<form action=new_user.html method=post>
     <input type='submit' value='Új felhasználó'/>
 	</form>";
-	//echo "<button onclick=window.location.href=/new_user.php>Continue</button>";
 
 	mysqli_close($con);
 }
---
+++ intranet/update_user.php
@@ -0,0 +1,15 @@
+<?php
+//csatlakozás db-hez és adatok frissítése, majd visszatérés
+session_start();
+include('config/dbconnect.php');
+if($_POST['disabled2'])
+{
+	$dis = 1;
+} else { $dis = 0;};
+
+$query="UPDATE users SET name='".$_POST['name2']."',username='".$_POST['username2']."',email='".$_POST['email2']."',role_id='".$_POST['role2']."',disabled='".$dis."' where ID = '".$_POST['id2']."'";
+echo $query;
+mysqli_query($con,$query) or die ("Nem sikerült".mysqli_error($con));
+mysqli_close($con);
+//echo '<meta http-equiv="refresh" content="0; URL=menu.php?d=2">';
+?>
